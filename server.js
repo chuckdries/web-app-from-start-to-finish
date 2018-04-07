7144d1fc8b36f3c35449c331f7fe45931b080668
Chuck Dries <chuck@chuckdries.com>
2018-04-06 23:50:06 -0700

    Switch to twig

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ var app = express();
 
 // Configure view engine to render EJS templates.
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'twig');
 
 // Use application-level middleware for common functionality, including
 // logging, parsing, and session handling.
